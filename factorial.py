d7cd696f36497ba43d2d32223004895fc8d88335
MattW-12106 <12106@holbertonstudents.com>
2026-01-20 10:00:50 +0000

    factorial

--- factorial.py
+++ factorial.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 import sys
 
 def factorial(n):
@@ -7,10 +7,6 @@ def factorial(n):
         result *= n
         n -= 1
     return result
-
-if len(sys.argv) != 2:
-    print("Usage: factorial n")
-    sys.exit(1)
 
 f = factorial(int(sys.argv[1]))
 print(f)
